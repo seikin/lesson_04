358f43883087d4c58a7ebc4f1bf401266ae3422e
seiki <seikin.alexey@gmail.com>
2021-10-24 20:30:18 +0300

    ok

--- src/lesson_04_hometask_review.kt
+++ src/lesson_04_hometask_review.kt
@@ -1,1 +1,10 @@
-dhfh
+fun main(){
+    val list01 = listOf<Any>(10, 11F, 12.5, 'T', "В","Ксанад","чертог","Кубла")
+    val list02 = listOf<Any>("Integer", "Float", "Double", "Char", "стране","благословенной","построил","Хан")
+
+    for (j in list01.indices){
+        println("${list01[j]} : ${list02[j]}, ")
+
+    }
+
+}
